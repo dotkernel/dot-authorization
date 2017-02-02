d295e2348762fcaf27f89d0c81cd40e9cb586339
n3vrax <n3vrax@gmail.com>
2017-02-02 20:25:10 +0200

    refactoring

--- src/Exception/ForbiddenException.php
+++ src/Exception/ForbiddenException.php
@@ -11,11 +11,16 @@ declare(strict_types = 1);
 
 namespace Dot\Authorization\Exception;
 
+use Exception;
+
 /**
  * Class ForbiddenException
  * @package Dot\Authorization\Exception
  */
 class ForbiddenException extends \Exception implements ExceptionInterface
 {
-
+    public function __construct($message = "", $code = 403, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
